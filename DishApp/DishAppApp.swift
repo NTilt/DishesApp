30bd2183f79309640bdad6d908f3a44ed261ad0b
Nikita <ya.n.2@mail.com>
2023-07-15 11:35:49 +0400

    Initial Commit

---
+++ DishApp/DishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishAppApp.swift
+//  DishApp
+//
+//  Created by Никита Ясеник on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
